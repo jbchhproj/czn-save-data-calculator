26a9da779e206efdb6ad1ab9923a35bbf1b41447
jbchhproj <jbchheav.project@gmail.com>
2026-03-27 22:09:54 -0500

    prettier

--- src/types/SelectionBlockConfig.tsx
+++ src/types/SelectionBlockConfig.tsx
@@ -4,6 +4,6 @@ export interface SelectionBlockConfig {
   tooltip: string; // Tooltip text to explain the block
   min: number; // Minimum allowed value for the block's counter
   max: number; // Maximum allowed value for the block's counter
-  rule: (current: number, action: "increment" | "decrement") => number;   // Function that determines how the value changes when incrementing or decrementing
-  contribution: (current: number) => number;   // Function that determines how this block's value contributes to the overall calculation
+  rule: (current: number, action: "increment" | "decrement") => number; // Function that determines how the value changes when incrementing or decrementing
+  contribution: (current: number) => number; // Function that determines how this block's value contributes to the overall calculation
 }

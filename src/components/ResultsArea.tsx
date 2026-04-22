310983bfb9438363f6aad07a816681ee1bdefd6e
jbchhproj <jbchheav.project@gmail.com>
2026-04-22 02:14:51 -0500

    change fm layout again

--- src/components/ResultsArea.tsx
+++ src/components/ResultsArea.tsx
@@ -1,5 +1,3 @@
-import SlashIcon from "./icons/SlashIcon";
-
 interface ResultsSectionProps {
   tier: number;
   totalFaintMemory: number;
@@ -10,23 +8,16 @@ export default function ResultsSection({
   totalFaintMemory,
 }: ResultsSectionProps) {
   const faintMemoryLimit = 20 + tier * 10;
-  const [leadingLabel, ...trailingLabelParts] = "FAINT MEMORY".split(/\s+/);
-  const trailingLabel = trailingLabelParts.join(" ");
 
   return (
     <div className="-ml-8 flex h-full w-[calc(100%+2rem)] items-center rounded-r bg-amber-200 py-1 pl-8 pr-2 text-sm text-indigo-950">
-      <div className="flex w-full items-center gap-3">
-        <div className="flex w-16 shrink-0 flex-col justify-center sm:w-24">
-          <span className="leading-tight">{leadingLabel}</span>
-          {trailingLabel && (
-            <span className="leading-tight">{trailingLabel}</span>
-          )}
-        </div>
+      <div className="flex w-full flex-col justify-center leading-tight">
+        <span>FAINT MEMORY</span>
 
         <div className="inline-flex items-center gap-1 whitespace-nowrap tabular-nums">
-          <span className="w-3 text-right">{totalFaintMemory}</span>
-          <SlashIcon className="h-4 w-4" />
-          <span className="w-3">{faintMemoryLimit}</span>
+          <span className="w-[5ch] text-right">{totalFaintMemory}</span>
+          <span className="w-[1ch] text-center">/</span>
+          <span className="w-[1ch] text-right">{faintMemoryLimit}</span>
         </div>
       </div>
     </div>

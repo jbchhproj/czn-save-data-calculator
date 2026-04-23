463ab2bc9ec82d5007f60a46d93f5c3a7a54b92d
jbchhproj <jbchheav.project@gmail.com>
2026-04-23 03:47:58 -0500

    warning conditional

--- src/components/ResultsArea.tsx
+++ src/components/ResultsArea.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 interface ResultsSectionProps {
   tier: number;
   totalFaintMemory: number;
@@ -10,11 +12,16 @@ export default function ResultsSection({
   const faintMemoryLimit = 20 + tier * 10;
 
   return (
-    <div className="-ml-8 flex h-full w-[calc(100%+2rem)] items-center rounded-r border-b-2 border-amber-400 bg-amber-200 py-1 pl-8 pr-2 text-sm text-slate-950 [font-family:var(--font-allerta-stencil)]">
-      <div className="flex w-full flex-col justify-center leading-tight text-lg">
+    <div className="-ml-8 flex h-full w-[calc(100%+2rem)] items-center rounded-r border-b-2 border-amber-400 bg-amber-200 py-1 pl-8 pr-2 text-lg text-slate-950 [font-family:var(--font-allerta-stencil)]">
+      <div className="flex w-full flex-col justify-center leading-tight">
         <span>FAINT MEMORY</span>
 
-        <div className="inline-flex items-center gap-1 whitespace-nowrap tabular-nums">
+        <div
+          className={clsx(
+            "inline-flex items-center gap-1 whitespace-nowrap tabular-nums",
+            totalFaintMemory > faintMemoryLimit && "text-red-500",
+          )}
+        >
           <span className="w-[5ch] text-right">{totalFaintMemory}</span>
           <span className="w-[1ch] text-center">/</span>
           <span className="w-[1ch] text-right">{faintMemoryLimit}</span>

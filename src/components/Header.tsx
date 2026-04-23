c2f3e5ca911dac34b546dc51665bd8eeb1b01d46
jbchhproj <jbchheav.project@gmail.com>
2026-04-23 03:26:52 -0500

    add border header elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,13 +22,13 @@ export default function Header({
   onReset,
 }: TierSelectorProps) {
   return (
-    <header className="sticky top-0 z-50 flex items-stretch gap-2 border-b-4 border-gray-400 bg-slate-600 p-2 shadow-md/30">
+    <header className="sticky top-0 z-50 flex items-stretch gap-2 bg-slate-600 p-2 shadow-md/30">
       <div className="min-w-0 flex-1">
         <ResultsArea tier={tier} totalFaintMemory={totalFaintMemory} />
       </div>
 
       <div className="flex flex-col gap-2">
-        <div className="flex h-9 items-center justify-end gap-3 rounded bg-gray-200 px-2">
+        <div className="flex h-9 items-center justify-end gap-3 rounded bg-slate-200 px-2 border-b-2 border-r-2 border-slate-400">
           <PhoneIcon />
           <ResetButton onReset={onReset} />
           <DeepTrauma
@@ -37,11 +37,13 @@ export default function Header({
           />
         </div>
 
-        <TierSelector
-          tier={tier}
-          setTier={setTier}
-          isDeepTraumaActive={isDeepTraumaActive}
-        />
+        <div className="rounded bg-slate-200 border-b-2 border-r-2 border-slate-400">
+          <TierSelector
+            tier={tier}
+            setTier={setTier}
+            isDeepTraumaActive={isDeepTraumaActive}
+          />
+        </div>
       </div>
     </header>
   );

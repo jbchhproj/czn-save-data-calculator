e6a59e8eb739cafa41408e9a89bf7175389ac5d5
jbchhproj <jbchheav.project@gmail.com>
2026-04-29 00:50:47 -0500

    change non touchable

--- src/components/selection/SelectionBlock.tsx
+++ src/components/selection/SelectionBlock.tsx
@@ -41,17 +41,17 @@ export default function SelectionBlock({
 
   return (
     <>
-      <div className="relative mt-3">
+      <div className="relative mt-3 select-none [-webkit-touch-callout:none]">
         {/* bottom outline (the "3D" effect) */}
         <div className="pointer-events-none absolute bottom-[-6px] left-[-0.5rem] right-0 h-3 rounded-br-lg border-b-2 border-gray-300 shadow-[0_2px_2px_rgba(0,0,0,0.20)]" />
 
         {/* main block */}
         <div className="-ml-2 flex h-17 w-[calc(100%+0.5rem)] items-center gap-3 overflow-hidden rounded-r bg-gray-400/30 px-1">
-          <div className="-ml-4 flex h-full w-30 shrink-0 select-none flex-col justify-center bg-gray-500/30 pl-5 text-slate-950 [-webkit-touch-callout:none]">
+          <div className="-ml-4 flex h-full w-30 shrink-0 flex-col justify-center bg-gray-500/30 pl-5 text-slate-950">
             {labelWordElements}
           </div>
 
-          <span className="pl-1 rounded bg-gray-100 text-md text-slate-950 border border-slate-300 select-none [-webkit-touch-callout:none] tabular-nums w-[7ch]">
+          <span className="pl-1 rounded bg-gray-100 text-md text-slate-950 border border-slate-300 tabular-nums w-[7ch]">
             {config.faintMemoryContribution(stepperCount)}
           </span>
 
